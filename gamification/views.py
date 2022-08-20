5770b19ec37fd47c61d0c2a7715c55a8e15d3f3b
shimizu1010 <10shimizu10+github@gmail.com>
2022-08-20 21:45:07 +0900

    retry

---
+++ gamification/views.py
@@ -0,0 +1,39 @@
+#ここではページを定義している
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+  template_name = 'gamification/index.html'
+  context_object_name = 'latest_question_list'
+
+  def get_queryset(self):
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+  model = Question
+  template_name = 'gamification/detail.html'
+  
+  def get_queryset(self):
+    return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+  model =Question
+  template_name = 'gamification/results.html'
+
+def vote(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'gamification/detail.html',{'question':question, 'error_message':'選択肢を選んでいない'}) 
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('gamification:results', args=(question.id)))
